6151f0d1ad2e04638eea33d677a092eee5c5f0ee
Thiag0h <thiagoth3005@gmail.com>
2025-03-18 13:29:27 -0300

    NAO SEI FAZER JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $d(document).ready(function() {
 $('#mobile_btn').on('click', function () {
-     $('navbar').toggleclass('active')
+     $('mobile_menu').toggleclass('active')
      $('#mobile_btn'). find('i'). toggleclass('fa-x')
 });
 });
