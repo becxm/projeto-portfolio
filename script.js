e1080d67bbaff2a6d5d6837763dfd6913bd6d710
Thiag0h <thiagoth3005@gmail.com>
2025-03-18 14:09:43 -0300

    upgrade
    to tentando deixar a pagina responsiva para o celular com o js, ta dificil.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $d(document).ready(function() {
-$('#mobile_btn').on('click', function () {
+$('.mobile_btn').on('click', function () {
      $('mobile_menu').toggleclass('active')
      $('#mobile_btn'). find('i'). toggleclass('fa-x')
 });
