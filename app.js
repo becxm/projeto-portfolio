1b16ea971f3d2fe238f82bc00c95b8623e1d6b33
Thiag0h <thiagoth3005@gmail.com>
2025-03-26 19:53:13 -0300

    V1.3

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 const menubar = document.querySelector(".menubar");
 
 menubar.addEventListener("click", () => nav.classList.toggle("active"))
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Fechar o modal ao clicar fora dele
+window.onclick = function(event) {
+    let modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
